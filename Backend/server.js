a313c77dc6010aa84c1678a3ac48185417abe9c8
Dragx21 <raghavpareek0321@gmail.com>
2025-06-12 23:17:52 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,21 @@ import dotenv from 'dotenv';
 import { connectdb } from './config/db.js';
 dotenv.config();
 const app=express();
-app.get('/',(req,res)=>{
-    res.send("Welcome to the Basic E-commerce API");
-})
+app.post('/products',async (req,res)=>{
+    const product=req.body;
+    if(!product.name ||!product.price ||!product.image){
+        return res.status(400).json({success:false,message:"Please give all fields"});
+    }
+    const newProduct= new Product(product)
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true,data:newProduct});
+    } catch (error) {
+        console.error("Error in product creation",error.message);
+        res.status(500).json({success:false,message:"Server Error"})
+        
+    }
+});
 
 app.listen(5000, () =>{
     connectdb()
